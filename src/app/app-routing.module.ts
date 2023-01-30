7b512cb3d73981bf544da819c9efec56454e1e97
shreyanshumalviya <shreyanshumalviya@gmail.com>
2023-01-30 19:43:48 +0530

    added unique route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrinterComponent } from './printer/printer.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'unique', component: PrinterComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
